36232a44556827246386f7a3bf92c47256ec377e
Hyury Luis da Silva Noll <hyury@weg.net>
2022-06-07 08:12:34 -0300

    ...

---
+++ messages/src/messages/messages.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { MessageRepository } from './messages.repository';
+
+@Injectable()
+export class MessagesService {
+  constructor(private messagesRepo: MessageRepository) {}
+
+  async findOne(id: string) {
+    return this.messagesRepo.findOne(id);
+  }
+
+  async findAll() {
+    return this.messagesRepo.findAll();
+  }
+
+  async create(content: string) {
+    return this.messagesRepo.create(content);
+  }
+}
